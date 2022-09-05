536a2a0297f5dcb95aceca3e84af4c1a0d1b2614
Loarlam <Loarlam@ya.ru>
2022-09-05 18:12:15 +0300

    Внесены правки в тесты по русскоязычным элементам

--- src/test/java/guru/qa/tests/AndroidSearchTests.java
+++ src/test/java/guru/qa/tests/AndroidSearchTests.java
@@ -6,20 +6,24 @@ import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Tag("android")
 public class AndroidSearchTests extends TestBase {
     @Test
     void searchTest() {
+//        switchTo().alert().accept();
         back();
+
         step("Type search", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
-            Thread.sleep(10000); //задержка нужна для поиска википедии, который тупит
+//            Thread.sleep(10000); //задержка нужна для поиска википедии, который тупит
         });
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
@@ -32,11 +36,12 @@ public class AndroidSearchTests extends TestBase {
     @DisplayName("Запуск мобильных автотестов при помощи Appium для поиска в вики значения \"dyskinesia\"")
     void searchTextWikiInSearchSrc() {
         back();
+//        switchTo().alert().accept();
         step("Поиск в поисковой строке значения dyskinesia", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.accessibilityId("Поиск по Википедии")).click();
             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
                     .sendKeys("Dyskinesia");
-            Thread.sleep(10000); //задержка нужна для поиска википедии, который тупит
+//            Thread.sleep(10000); //задержка нужна для поиска википедии, который тупит
         });
         step("Проверка найденного контента dyskinesia", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
@@ -48,6 +53,8 @@ public class AndroidSearchTests extends TestBase {
     @Severity(SeverityLevel.NORMAL)
     @DisplayName("Запуск мобильных автотестов при помощи Appium для поиска в вики значения \"aplasia\"")
     void searchTextWikiInSearchContatiner() {
+        back();
+//        switchTo().alert().accept();
         step("Поиск в search_container значения Aplasia", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Aplasia");
@@ -55,7 +62,7 @@ public class AndroidSearchTests extends TestBase {
         step("Проверка найденного контента Aplasia", () -> {
             $$(AppiumBy.id("org.wikipedia.alpha:id/search_results_list")).shouldHave(
                     CollectionCondition.sizeGreaterThan(0));
-            Thread.sleep(10000); //задержка нужна для поиска википедии, который тупит
+//            Thread.sleep(10000); //задержка нужна для поиска википедии, который тупит
         });
         step("Открыть страницу Aplasia", () -> {
             $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
