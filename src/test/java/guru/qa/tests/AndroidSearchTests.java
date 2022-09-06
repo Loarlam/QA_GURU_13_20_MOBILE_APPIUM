a06438ad8e59f48b7cb585acedf5fe4ad1d40940
Loarlam <Loarlam@ya.ru>
2022-09-06 19:39:52 +0300

    Дополнен выбор в TestBase для разных девайсов. Поправлены тесты.

--- src/test/java/guru/qa/tests/AndroidSearchTests.java
+++ src/test/java/guru/qa/tests/AndroidSearchTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -17,33 +18,58 @@ import static io.qameta.allure.Allure.step;
 public class AndroidSearchTests extends TestBase {
     @Test
     @Severity(SeverityLevel.NORMAL)
-    @DisplayName("Запуск мобильных автотестов при помощи Appium для поиска в вики значения \"\"")
-    void searchTest() {
+    @DisplayName("Проверка кнопки search в мп Wikipedia")
+    void savedButtonTest() {
 //        switchTo().alert().accept();
         back();
 
-        step("Type search", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
-        });
-        step("Verify content found", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(CollectionCondition.sizeGreaterThan(0)));
+        step("Нажатие на кнопку saved", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/nav_tab_reading_lists")).click());
+        step("Проверка открытия верной страницы", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/messageTitleView")).shouldHave(text("Sync reading lists")));
     }
-
 
     @Test
     @Severity(SeverityLevel.NORMAL)
-    @DisplayName("Запуск мобильных автотестов при помощи Appium для поиска в вики значения \"dyskinesia\"")
+    @DisplayName("Проверка кнопки search в мп Wikipedia")
+    void searchButtonTest() {
+//        switchTo().alert().accept();
+        back();
+
+        step("Нажатие на кнопку search", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/nav_tab_search")).click());
+        step("Проверка открытия верной страницы", () ->
+        {
+            $(AppiumBy.id("org.wikipedia.alpha:id/history_title")).shouldHave(text("History"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/history_empty_container")).shouldHave(text("No recently viewed articles"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/history_empty_message")).shouldHave(text("Track what you've been reading here."));
+        });
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Проверка кнопки search в мп Wikipedia")
+    void editsButtonTest() {
+//        switchTo().alert().accept();
+        back();
+
+        step("Нажатие на кнопку edits", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/nav_tab_edits")).click());
+        step("Проверка открытия верной страницы", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/messageTitleView")).shouldHave(text("Did you know that everyone can edit Wikipedia?")));
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Клик в поле search wikipedia и поиск \"dyskinesia\"")
     void searchTextWikiInSearchSrc() {
         back();
 //        switchTo().alert().accept();
-        step("Поиск в поисковой строке значения dyskinesia", () -> {
-            $(AppiumBy.accessibilityId("Поиск по Википедии")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))
-                    .sendKeys("Dyskinesia");
-        });
-        step("Проверка найденного контента dyskinesia", () ->
+        step("Клик в поле search wikipedia", () ->
+                $(AppiumBy.accessibilityId("Search Wikipedia")).click());
+        step("Ввод в поле search wikipedia значения \"dyskinesia\"", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))).sendKeys("Dyskinesia");
+        step("Проверка найденного контента \"dyskinesia\"", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
     }
@@ -51,20 +77,18 @@ public class AndroidSearchTests extends TestBase {
     @Test
     @Owner("Loarlam")
     @Severity(SeverityLevel.NORMAL)
-    @DisplayName("Запуск мобильных автотестов при помощи Appium для поиска в вики значения \"aplasia\"")
+    @DisplayName("Клик в поле search wikipedia и поиск \"aplasia\"")
     void searchTextWikiInSearchContatiner() {
         back();
 //        switchTo().alert().accept();
-        step("Поиск в search_container значения Aplasia", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Aplasia");
-        });
-        step("Проверка найденного контента Aplasia", () -> {
-            $$(AppiumBy.id("org.wikipedia.alpha:id/search_results_list")).shouldHave(
-                    CollectionCondition.sizeGreaterThan(0));
-        });
-        step("Открыть страницу Aplasia", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-        });
+        step("Клик в поле search wikipedia", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click());
+        step("Ввод в поле search wikipedia значения \"aplasia\"", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text"))).sendKeys("Aplasia");
+        step("Проверка найденного контента \"aplasia\"", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/search_results_list"))
+                        .shouldHave(CollectionCondition.sizeGreaterThan(0)));
+        step("Открытие страницы Aplasia", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click());
     }
 }
--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -2,7 +2,9 @@ package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import guru.qa.drivers.BrowserstackMobileDriver;
 import guru.qa.drivers.PersonalMobileDriver;
+import guru.qa.drivers.VirtualMobileDriver;
 import guru.qa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -11,12 +13,24 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static guru.qa.helpers.Attach.*;
 import static io.qameta.allure.Allure.step;
 
 public class TestBase {
     @BeforeAll
     public static void setup() {
-        Configuration.browser = PersonalMobileDriver.class.getName();
+        switch (System.getProperty("deviceHost")) {
+            case ("browserstackDevice"):
+                Configuration.browser = BrowserstackMobileDriver.class.getName();
+                break;
+            case ("personalDevice"):
+                Configuration.browser = PersonalMobileDriver.class.getName();
+                break;
+            case ("virtualDevice"):
+                Configuration.browser = VirtualMobileDriver.class.getName();
+                break;
+        }
+
         Configuration.browserSize = null;
     }
 
@@ -29,8 +43,11 @@ public class TestBase {
 
     @AfterEach
     public void afterEach() {
+        String sessionId = sessionId();
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         step("Close driver", Selenide::closeWebDriver);
+        if (System.getProperty("deviceHost").equals("browserstack"))
+            Attach.video(sessionId);
     }
 }
